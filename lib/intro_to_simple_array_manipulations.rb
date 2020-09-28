804c082aa98b8bd57e69972a9f5e1fded01cc9b3
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-28 18:22:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,12 +10,12 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args
-  
+def pop_with_args(array)
+  array.pop(2)
 end 
 
-def using_shift
-  
+def using_shift(array, string)
+  array.shift(string)
 end 
 
 def shift_with_args
